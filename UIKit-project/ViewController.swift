54cc29a9b028ce899cf6c81bb4a56eda717c1465
alibekshak <alibekshakiro1@gmail.com>
2025-11-03 09:06:36 +0500

    project

---
+++ UIKit-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-project
+//
+//  Created by Alibek Shakirov on 03.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
