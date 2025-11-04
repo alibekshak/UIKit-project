b66a3e780740a4c4900a73430d499cdb1ea1d60f
alibekshak <alibekshakiro1@gmail.com>
2025-11-04 17:24:36 +0500

    Created TabBarController

---
+++ UIKit-project/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  UIKit-project
+//
+//  Created by Alibek Shakirov on 04.11.2025.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+}
