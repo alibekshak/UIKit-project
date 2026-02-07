87ecf6b9c2e535c59e7015574bbce1e0b3d52a91
alibekshak <alibekshakiro1@gmail.com>
2026-02-07 17:51:52 +0500

    Save phone and email

--- UIKit-project/Commons/Managers/UserDefaults/UserDataManager.swift
+++ UIKit-project/Commons/Managers/UserDefaults/UserDataManager.swift
@@ -11,25 +11,63 @@ final class UserDataManager: UserDataManagerProtocol {
     
     private enum Keys {
         static let username = "savedUsername"
+        static let phone = "savedPhone"
+        static let email = "savedEmail"
     }
     
-    private let userDefault = UserDefaults.standard
+    private let userDefaults = UserDefaults.standard
     
     // MARK: - Username Management
     
     func saveUsername(_ username: String) {
-        userDefault.set(username, forKey: Keys.username)
+        userDefaults.set(username, forKey: Keys.username)
     }
     
     func getUsername() -> String? {
-        return userDefault.string(forKey: Keys.username)
+        return userDefaults.string(forKey: Keys.username)
     }
     
     func hasUsername() -> Bool {
-        return userDefault.string(forKey: Keys.username) != nil
+        return userDefaults.string(forKey: Keys.username) != nil
     }
     
     func removeUsername() {
-        userDefault.removeObject(forKey: Keys.username)
+        userDefaults.removeObject(forKey: Keys.username)
+    }
+        
+    // MARK: - Phone
+
+    func savePhone(_ phone: String) {
+        userDefaults.set(phone, forKey: Keys.phone)
+    }
+
+    func getPhone() -> String? {
+        userDefaults.string(forKey: Keys.phone)
+    }
+
+    func hasPhone() -> Bool {
+        userDefaults.string(forKey: Keys.phone) != nil
+    }
+
+    func removePhone() {
+        userDefaults.removeObject(forKey: Keys.phone)
+    }
+
+    // MARK: - Email
+
+    func saveEmail(_ email: String) {
+        userDefaults.set(email, forKey: Keys.email)
+    }
+
+    func getEmail() -> String? {
+        userDefaults.string(forKey: Keys.email)
+    }
+
+    func hasEmail() -> Bool {
+        userDefaults.string(forKey: Keys.email) != nil
+    }
+
+    func removeEmail() {
+        userDefaults.removeObject(forKey: Keys.email)
     }
 }
--- UIKit-project/Commons/Managers/UserDefaults/UserDataManagerProtocol.swift
+++ UIKit-project/Commons/Managers/UserDefaults/UserDataManagerProtocol.swift
@@ -8,8 +8,21 @@
 import Foundation
 
 protocol UserDataManagerProtocol: AnyObject {
+    // Username
     func saveUsername(_ username: String)
     func getUsername() -> String?
     func hasUsername() -> Bool
     func removeUsername()
+    
+    // Phone
+    func savePhone(_ phone: String)
+    func getPhone() -> String?
+    func hasPhone() -> Bool
+    func removePhone()
+
+    // Email
+    func saveEmail(_ email: String)
+    func getEmail() -> String?
+    func hasEmail() -> Bool
+    func removeEmail()
 }
